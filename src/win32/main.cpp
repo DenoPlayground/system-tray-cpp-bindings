a0fa294bad50327efdbe8a1dbc16134c57ce697c
Mqxx <62719703+Mqxx@users.noreply.github.com>
2025-04-16 14:11:56 +0200

    init

---
+++ src/win32/main.cpp
@@ -0,0 +1,82 @@
+#include <windows.h>
+#include <shellapi.h>
+
+// Globale Variablen
+static NOTIFYICONDATA nid = {0};
+static HWND hWnd = NULL;
+static void (*leftClickCallback)() = NULL;
+static void (*rightClickCallback)() = NULL;
+
+// Fensterprozedur für Tray-Nachrichten
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+  if (msg == WM_APP + 1)
+  {
+    if (lParam == WM_LBUTTONDOWN && leftClickCallback)
+    {
+      leftClickCallback();
+    }
+    else if (lParam == WM_RBUTTONDOWN && rightClickCallback)
+    {
+      rightClickCallback();
+    }
+  }
+  else if (msg == WM_DESTROY)
+  {
+    Shell_NotifyIcon(NIM_DELETE, &nid);
+    PostQuitMessage(0);
+  }
+  return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+// Erstellt ein verstecktes Fenster
+HWND CreateHiddenWindow()
+{
+  WNDCLASS wc = {0};
+  wc.lpfnWndProc = WndProc;
+  wc.hInstance = GetModuleHandle(NULL);
+  wc.lpszClassName = "TrayWindowClass";
+  RegisterClass(&wc);
+
+  HWND hWnd = CreateWindow(
+      "TrayWindowClass", NULL, 0, 0, 0, 0, 0, NULL, NULL, wc.hInstance, NULL);
+  return hWnd;
+}
+
+// DLL-Einstiegspunkt
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+  if (ul_reason_for_call == DLL_PROCESS_ATTACH && !hWnd)
+  {
+    hWnd = CreateHiddenWindow();
+    nid.cbSize = sizeof(NOTIFYICONDATA);
+    nid.hWnd = hWnd;
+    nid.uID = 1;
+    nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+    nid.uCallbackMessage = WM_APP + 1;
+    nid.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    strcpy(nid.szTip, "Deno Tray App");
+  }
+  return TRUE;
+}
+
+// Exportierte Funktionen
+extern "C" __declspec(dllexport) void AddTrayIcon()
+{
+  Shell_NotifyIcon(NIM_ADD, &nid);
+}
+
+extern "C" __declspec(dllexport) void RemoveTrayIcon()
+{
+  Shell_NotifyIcon(NIM_DELETE, &nid);
+}
+
+extern "C" __declspec(dllexport) void SetLeftClickCallback(void (*callback)())
+{
+  leftClickCallback = callback;
+}
+
+extern "C" __declspec(dllexport) void SetRightClickCallback(void (*callback)())
+{
+  rightClickCallback = callback;
+}
